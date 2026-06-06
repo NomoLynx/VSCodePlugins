8c520cb5bb0bbff33b24d16ceb7fa15d221717fd
Tao <ttliu2000@gmail.com>
2026-06-06 13:14:41 -0400

    adding memory functions

--- debugger/src/machine/machine.rs
+++ debugger/src/machine/machine.rs
@@ -1,5 +1,7 @@
 use riscv_asm_lib::r5asm::asm_program::AsmProgram;
 use riscv_asm_lib::r5asm::instruction::Instruction;
+use riscv_asm_lib::r5asm::opcode::OpCode;
+use riscv_asm_lib::r5asm::register::Register;
 
 use crate::debugger_error::DebuggerError;
 use crate::machine::hart::Hart;
@@ -16,9 +18,21 @@ pub struct Machine {
     pub programs: Vec<AsmProgram>,
 
     pub memory: Memory,
+
+    pub registers: Register,
 }
 
 impl Machine {
+
+    pub fn new() -> Self {
+        Self {
+            processors: vec![],
+            programs: vec![],
+            memory: Memory::new(),
+
+            registers: Register::new(),
+        }
+    }
 
     pub fn add_program(
         &mut self,
@@ -40,11 +54,22 @@ impl Machine {
         self.processors.push(processor);
     }
 
-    pub fn get_hart_mut(
-        &mut self,
-        hart_id: HartId,
-    ) -> Option<&mut Hart> {
+    pub fn get_hart(&self, hart_id: HartId) -> Option<&Hart> {
+        for processor in &self.processors {
 
+            for hart in &processor.harts {
+
+                if hart.id == hart_id {
+
+                    return Some(hart);
+                }
+            }
+        }
+
+        None
+    }
+
+    pub fn get_hart_mut(&mut self, hart_id: HartId) -> Option<&mut Hart> {
         for processor in &mut self.processors {
 
             for hart in &mut processor.harts {
@@ -97,10 +122,71 @@ impl Machine {
 
     }
 
+    fn xreg(
+        &self,
+        name: &Option<String>,
+    ) -> usize {
+
+        self.registers
+            .get_register_value(name.as_ref())
+            .unwrap() as usize
+    }
+
+    fn get_x(
+        &self,
+        hart_id: HartId,
+        reg: Option<&String>,
+    ) -> u64 {
+
+        let idx =
+            self.registers
+                .get_register_value(reg)
+                .unwrap() as usize;
+
+        let hart = self.get_hart(hart_id).unwrap();
+        hart.x.regs[idx].value
+    }
+
+    fn set_x(
+        &mut self,
+        hart_id: HartId,
+        reg: Option<&String>,
+        value: u64,
+    ) {
+
+        let idx =
+            self.registers
+                .get_register_value(reg)
+                .unwrap() as usize;
+
+        let hart = self.get_hart_mut(hart_id).unwrap();
+        if idx != 0 {
+            hart.x.regs[idx].value = value;
+        }
+    }
+
     fn execute_inst(&mut self, hart_id: HartId, inst: &Instruction) -> Result<(), DebuggerError> {
-        let hart = self.get_hart_mut(hart_id as u64).unwrap();
         let opcode = inst.get_op_code()
                     .map_err(|x| DebuggerError::GeneralError(x.get_error_message()))?;
+
+        match opcode {
+            OpCode::Add => {
+                let lhs = self.get_x(hart_id, inst.get_r1());
+
+                let rhs = self.get_x(hart_id, inst.get_r2());
+
+                self.set_x(
+                    hart_id,
+                    inst.get_r0(),
+                    lhs.wrapping_add(rhs),
+                );
+
+                self.get_hart_mut(hart_id).unwrap().next_pc();
+            }
+            _ => {
+                return Err(DebuggerError::GeneralError(format!("unsupported instruction: {:?}", opcode)));
+            }
+        }
 
         Ok(())
     }
--- debugger/src/memory/memory.rs
+++ debugger/src/memory/memory.rs
@@ -1,3 +1,124 @@
 pub struct Memory {
     pub bytes: Vec<u8>,
 }
+
+impl Default for Memory {
+    fn default() -> Self {
+        Self {
+            bytes: vec![0; 64 * 1024 * 1024],
+        }
+    }
+}
+
+impl Memory {
+
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn with_size(
+        size: usize,
+    ) -> Self {
+
+        Self {
+            bytes: vec![0; size],
+        }
+    }
+
+    pub fn read_u8(
+        &self,
+        addr: usize,
+    ) -> u8 {
+
+        self.bytes[addr]
+    }
+
+    pub fn write_u8(
+        &mut self,
+        addr: usize,
+        value: u8,
+    ) {
+
+        self.bytes[addr] = value;
+    }
+
+    pub fn read_u32(
+        &self,
+        addr: usize,
+    ) -> u32 {
+
+        let bytes = [
+            self.bytes[addr],
+            self.bytes[addr + 1],
+            self.bytes[addr + 2],
+            self.bytes[addr + 3],
+        ];
+
+        u32::from_le_bytes(bytes)
+    }
+
+    pub fn write_u32(
+        &mut self,
+        addr: usize,
+        value: u32,
+    ) {
+
+        let bytes = value.to_le_bytes();
+
+        self.bytes[addr]     = bytes[0];
+        self.bytes[addr + 1] = bytes[1];
+        self.bytes[addr + 2] = bytes[2];
+        self.bytes[addr + 3] = bytes[3];
+    }
+
+    pub fn read_u64(
+        &self,
+        addr: usize,
+    ) -> u64 {
+
+        let bytes = [
+            self.bytes[addr],
+            self.bytes[addr + 1],
+            self.bytes[addr + 2],
+            self.bytes[addr + 3],
+            self.bytes[addr + 4],
+            self.bytes[addr + 5],
+            self.bytes[addr + 6],
+            self.bytes[addr + 7],
+        ];
+
+        u64::from_le_bytes(bytes)
+    }
+
+    pub fn write_u64(
+        &mut self,
+        addr: usize,
+        value: u64,
+    ) {
+
+        let bytes = value.to_le_bytes();
+
+        self.bytes[addr]     = bytes[0];
+        self.bytes[addr + 1] = bytes[1];
+        self.bytes[addr + 2] = bytes[2];
+        self.bytes[addr + 3] = bytes[3];
+        self.bytes[addr + 4] = bytes[4];
+        self.bytes[addr + 5] = bytes[5];
+        self.bytes[addr + 6] = bytes[6];
+        self.bytes[addr + 7] = bytes[7];
+    }
+
+    pub fn read_i32(
+        &self,
+        addr: usize,
+    ) -> i32 {
+        self.read_u32(addr) as i32
+    }
+
+    pub fn read_i64(
+        &self,
+        addr: usize,
+    ) -> i64 {
+        self.read_u64(addr) as i64
+    }
+}
