1cf2893d588d54ee8f475ce54eeb6ed40f71e800
Tao <ttliu2000@gmail.com>
2026-06-12 10:16:59 -0400

    added test for sim

--- debugger/build.rs
+++ debugger/build.rs
@@ -6,6 +6,6 @@ use copy_to_output::copy_to_output;
 fn main() {
     let build_type = &env::var("PROFILE").unwrap();
 
-    println!("cargo:return-if-changed=src/code.asm");
-    copy_to_output("src/code.asm", build_type).expect("Failed to copy code.asm to output directory");
+    println!("cargo:return-if-changed=tests/code.asm");
+    copy_to_output("tests/code.asm", build_type).expect("Failed to copy code.asm to output directory");
 }
--- debugger/tests/test.rs
+++ debugger/tests/test.rs
@@ -1,19 +1,33 @@
-mod machine;
-mod memory;
-mod vm;
-mod trace;
-mod debug;
-mod debugger_error;
-
 use core_utils::filesystem::read_file_to_string;
 use riscv_asm_lib::r5asm::assembler::*;
+use std::path::PathBuf;
+
+#[path = "../src/machine/mod.rs"]
+mod machine;
+#[path = "../src/memory/mod.rs"]
+mod memory;
+#[path = "../src/vm/mod.rs"]
+mod vm;
+#[path = "../src/trace/mod.rs"]
+mod trace;
+#[path = "../src/debug/mod.rs"]
+mod debug;
+#[path = "../src/debugger_error.rs"]
+mod debugger_error;
 
 use crate::machine::machine::Machine;
 
-fn main() {
+#[test]
+fn test_machine_execution() {
+    // Get the manifest directory and construct path to code.asm
+    let manifest_dir = env!("CARGO_MANIFEST_DIR");
+    let asm_file_path = PathBuf::from(manifest_dir)
+        .join("tests")
+        .join("code.asm");
+    
     let mut machine = Machine::default();
 
-    let asm_prog_text = read_file_to_string("./tests/code.asm");
+    let asm_prog_text = read_file_to_string(asm_file_path.to_str().unwrap());
     let program = parse_asm_use_default_config(&asm_prog_text).unwrap();
     machine.add_program(program);
 
@@ -24,16 +38,17 @@ fn main() {
     let default_hart_id = machine.get_default_hart_id();
     machine.init_hart_to_entry_point(default_hart_id).unwrap();
 
-    let hart = machine.get_default_hart_mut().unwrap();
+    let hart = machine.get_default_hart_mut().unwrap();    
     hart.write_register(&t1, 10.into());
     hart.write_register(&t2, 20.into());
+    
 
     let r = machine.step_hart(default_hart_id);
     let result = machine.get_default_hart().unwrap().read_register(&t0);
-
-    println!("Result: {:?}", r);
-    println!("t0: {:?}", result);
+    
+    assert!(r.is_ok());
+    assert_eq!(result, 30.into());
 
     let r = machine.step_hart(default_hart_id);
-    println!("Result: {:?}", r);
+    assert!(r.is_ok());
 }
